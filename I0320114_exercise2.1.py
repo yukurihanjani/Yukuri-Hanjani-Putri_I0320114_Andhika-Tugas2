130f11ebc769ecc64715224749f092d040d96ac3
Yukuri Hanjani <hanjaniyukuri@gmail.com>
2021-03-11 21:07:08 +0700

    Exercise

--- I0320114_exercise2.1.py
+++ I0320114_exercise2.1.py
@@ -1,1 +1,18 @@
+#tipe data Boolean
+print(True)
 
+#tipe data String
+print("Ayo belajar Python")
+print("Belajar Python Sangat Mudah")
+
+#tipe data Integer
+print(20)
+
+#tipe data Float
+print(3.14)
+
+#tipe data Hexadecimal
+print(9a)
+
+#tipe data Complex
+print(5j)
--- I0320114_exercise2.4.py
+++ I0320114_exercise2.4.py
@@ -1,1 +1,13 @@
+#menampilkan informasi program
 
+print("Konversi suhu (Fahrenheit ke Celcius)")
+
+#input suhu dalam fahrenheit
+F = float(input("Masukkan sushu(fahrenheit): "))
+
+#melakukan konversi suhu ke celcius
+C = 5 * (F-32) / 9
+
+#menampilkan hasil konversi ke layar
+print("Fahrenheit: ", F)
+print("Celcius: ", C)
