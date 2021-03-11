085b3d1597c2d246ffc48233d917e3d09d40e437
Yukuri Hanjani <hanjaniyukuri@gmail.com>
2021-03-11 22:47:26 +0700

    Exercise 2

--- I0320114_soal1_tugas2.py
+++ I0320114_soal1_tugas2.py
@@ -1,1 +1,71 @@
+### Hitung Luas Persegi Panjang
 
+# informasi program
+print("===== Hitung Luas Persegi Panjang =====")
+
+# input panjang dan lebar
+panjang = float(input("Memasukan Panjang: "))
+lebar = float(input("Memasukan Lebar: "))
+
+# proses hitung luas
+luasPersegiPanjang = panjang * lebar
+
+# menampilkan hasil
+print("Luas Persegi Panjang =", luasPersegiPanjang, "\n")
+
+### Hitung Luas Lingkaran
+
+# informasi program
+print("===== Hitung Luas Lingkaran =====")
+
+# input jari-jari
+r = float(input("Memasukan Jari-Jari: "))
+
+# proses hitung luas
+
+luasLingkaran = 3.14 * (r ** 2)
+
+# menampilkan hasil
+print("Luas lingkaran =", luasLingkaran, "\n")
+
+### Hitung Luas Kubus
+
+# informasi program
+print("===== Hitung Luas Kubus =====")
+
+# input rusuk
+s = float(input("Memasukan Panjang Rusuk: "))
+
+# proses hitung luas
+luasKubus = 6 * (s ** 2)
+
+# menampilkan hasil
+print("Luas kubus =", luasKubus, "\n")
+
+### Menghitung konversi suhu celcius ke fahrenheit
+
+# informasi program
+print("===== Konversi suhu celcius ke fahrenheit =====")
+
+# input celcius
+celcius = float(input("Masukkan suhu celcius: "))
+
+# proses hitung celcius ke fahrenheit
+fahrenheit = (celcius * (9/5))+ 32
+
+# menampilkan hasil
+print(celcius, "celcius =", fahrenheit, "fahrenheit", "\n")
+
+### Menghitung konversi suhu reamur ke kelvin
+
+# informasi program
+print("===== Konversi suhu reamur ke kelvin =====")
+
+# input reamur
+reamur = float(input("Masukkan suhu reamur: "))
+
+# proses hitung celcius ke fahrenheit
+kelvin = (reamur * (5/4)) + 273.15
+
+# menampilkan hasil
+print(reamur, "reamur =", kelvin, "kelvin")
--- I0320114_soal2_tugas2.py
+++ I0320114_soal2_tugas2.py
@@ -1,1 +1,53 @@
+# Buat Variabel
 
+Nama = "Yukuri Hanjani Putri"
+Nama_panggilan = "Yuku"
+SMA = "SMA N 7 Yogyakarta"
+Hobi = "Menggambar"
+Jurusan = "Teknik Industri"
+Institusi = "Universitas Sebelas Maret"
+Idola = "Steve Jobs"
+
+Tempat_lahir = "Bandar Lampung"
+Alamat = "Bantul, Daerah Istimewa Yogyakarta"
+
+Tinggi_badan = 166
+Berat_badan = 60
+Ukuran_sepatu = 40
+
+Minuman_kesukaan = "Es Teh"
+Makanan_kesukaan = "Magelangan"
+Warna_kesukaan = "Biru"
+
+# Tanggal, bulan, dan tahun lahir
+TanggalLahir = 3
+BulanLahir = 10            # 10 = Oktober
+TahunLahir = 2001
+TanggalSkrg = 11
+BulanSkrg = 3
+TahunSkrg = 2021
+
+#Menghitung Usia (Tahun dan Bulan)
+Usia_Dalam_Bulan = ((12 * TahunSkrg + BulanSkrg) - (12 * TahunLahir + BulanLahir))
+Usia_Tahun = int(Usia_Dalam_Bulan/12)
+Usia_Bulan = Usia_Dalam_Bulan % 12
+Usia = str(Usia_Tahun) + " Tahun " + str(Usia_Bulan) + " Bulan"
+
+#tampilan
+print("==== BIODATA ====")
+print("Nama :", Nama)
+print("Usia :", Usia)
+print("Nama Panggilan :", Nama_panggilan)
+print("SMA :", SMA)
+print("Hobi :", Hobi)
+print("Jurusan :", Jurusan)
+print("Institusi :", Institusi)
+print("Idola :", Idola)
+print("Tempat Lahir :", Tempat_lahir)
+print("Alamat :", Alamat)
+print("Tinggi Badan :", Tinggi_badan, "cm")
+print("Berat Badan :", Berat_badan, "kg")
+print("Ukuran Sepatu :", Ukuran_sepatu)
+print("Makanan Kesukaan :", Makanan_kesukaan)
+print("Minuman Kesukaan :", Minuman_kesukaan)
+print("Warna Kesukaan :", Warna_kesukaan)
